48deca80b9129ee4b1fd2cc3c395fb49d4d0d73a
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-04-26 19:57:12 +0000

    URLConverter now throws an exception if a classpath resource does not exist

--- src/main/java/de/cosmocode/palava/core/inject/URLConverter.java
+++ src/main/java/de/cosmocode/palava/core/inject/URLConverter.java
@@ -22,6 +22,7 @@ import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.inject.spi.TypeConverter;
 
 /**
@@ -40,7 +41,10 @@ public final class URLConverter extends AbstractTypeConverter<URL> {
     public URL convert(String value) {
         if (value.startsWith(CLASSPATH_PREFIX)) {
             LOG.trace("Considering {} to be a classpath resource", value);
-            return getClassLoader().getResource(value.substring(CLASSPATH_PREFIX.length()));
+            final ClassLoader loader = getClassLoader();
+            final URL url = loader.getResource(value.substring(CLASSPATH_PREFIX.length()));
+            Preconditions.checkArgument(url != null, "%s is not pointing to a valid properties source", value);
+            return url;
         } else {
             try {
                 return new URL(value);
