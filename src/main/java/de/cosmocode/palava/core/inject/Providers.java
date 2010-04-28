d4131206125b3f249864cd2904e00cfb6117e5a3
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-04-28 14:19:12 +0000

    added Providers

---
+++ src/main/java/de/cosmocode/palava/core/inject/Providers.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.cosmocode.palava.core.inject;
+
+import com.google.inject.Provider;
+
+/**
+ * Static utility class for {@link Provider}s.
+ *
+ * @since 2.4
+ * @author Willi Schoenborn
+ */
+public final class Providers {
+    
+    private Providers() {
+        
+    }
+
+    /**
+     * Creates a {@link Provider} of type T which always returns null.
+     * 
+     * @param <T> generic return type
+     * @return a provider which always returns null
+     */
+    public static <T> Provider<T> nullProvider() {
+        return new NullProvider<T>();
+    }
+    
+    /**
+     * {@link Provider} implementation which always returns null.
+     *
+     * @author Willi Schoenborn
+     * @param <T> generic return type
+     */
+    private static final class NullProvider<T> implements Provider<T> {
+        
+        @Override
+        public T get() {
+            return null;
+        }
+        
+    }
+    
+}
