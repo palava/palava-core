4c1f035f686393b0d1126e2c0718b58bbe55df10
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-05-12 11:55:49 +0000

    added ProxyModule

---
+++ src/main/java/de/cosmocode/palava/core/inject/ProxyModule.java
@@ -0,0 +1,98 @@
+package de.cosmocode.palava.core.inject;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Provider;
+
+import de.cosmocode.palava.core.Registry;
+import de.cosmocode.palava.core.Registry.Proxy;
+import de.cosmocode.palava.core.Registry.SilentProxy;
+
+/**
+ * A {@link Module} which can be used to bind dynamic proxies
+ * provided by the current {@link Registry}.
+ *
+ * @since 
+ * @author Willi Schoenborn
+ */
+public abstract class ProxyModule extends AbstractModule {
+
+    /**
+     * Binds a dynamic proxy for the specified type which can
+     * be injected using the following code.
+     * {@code @Proxy Interface instance}
+     * 
+     * <p>
+     *   The same restrictions mentioned on {@link Registry#proxy(Class)}
+     *   apply to the provided type.
+     * </p>
+     * 
+     * @since 2.4
+     * @param <T> the generic proxy type
+     * @param type the proxy's class literal
+     * @throws NullPointerException if type is null
+     */
+    protected <T> void bindProxy(Class<T> type) {
+        Preconditions.checkNotNull(type, "Type");
+        bind(type).annotatedWith(Proxy.class).toProvider(new ProxyProvider<T>(
+            getProvider(Registry.class), type
+        ));
+    }
+
+    /**
+     * Binds a dynamic silent proxy for the specified type which can
+     * be injected using the following code.
+     * {@code @SilentProxy Interface instance}
+     * 
+     * <p>
+     *   The same restrictions mentioned on {@link Registry#silentProxy(Class)}
+     *   apply to the provided type.
+     * </p>
+     * 
+     * @since 2.4
+     * @param <T> the generic proxy type
+     * @param type the proxy's class literal
+     * @throws NullPointerException if type is null
+     */
+    protected <T> void bindSilentProxy(Class<T> type) {
+        Preconditions.checkNotNull(type, "Type");
+        bind(type).annotatedWith(SilentProxy.class).toProvider(new ProxyProvider<T>(
+            getProvider(Registry.class), type, true
+        ));
+    }
+    
+    /**
+     * Internal provider for dynamic proxies.
+     *
+     * @since 1.0
+     * @author Willi Schoenborn
+     * @param <T> the generic proxy type
+     */
+    private static final class ProxyProvider<T> implements Provider<T> {
+
+        private final Provider<Registry> provider;
+        
+        private final Class<T> type;
+        
+        private final boolean silent;
+
+        public ProxyProvider(Provider<Registry> provider, Class<T> type) {
+            this(provider, type, false);
+        }
+
+        public ProxyProvider(Provider<Registry> provider, Class<T> type, boolean silent) {
+            this.provider = Preconditions.checkNotNull(provider, "Provider");
+            this.type = Preconditions.checkNotNull(type, "Type");
+            this.silent = silent;
+        }
+        
+        @Override
+        public T get() {
+            final Registry registry = provider.get();
+            return silent ? registry.silentProxy(type) : registry.proxy(type);
+        }
+        
+    }
+    
+}
